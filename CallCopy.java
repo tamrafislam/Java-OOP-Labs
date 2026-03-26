23a5c695a26f2d18978a8c14c9414020e3a87547
tamrafislam <tam.raf.islam@gmail.com>
2026-03-26 15:46:54 +0000

    Add a copy of the source code

---
+++ CallCopy.java
@@ -0,0 +1,8 @@
+class Call {
+    public static void printHello () {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
